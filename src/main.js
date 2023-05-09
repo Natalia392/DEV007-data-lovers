4a118a6f924ceefe8dd1fc5a5e786f862ddef312
Natalia392 <ntorrejonvalencia@gmail.com>
2023-05-09 09:21:35 -0400

    Agrega botones de filtro (sin funcionalidad) y cambia for por forEach

--- src/main.js
+++ src/main.js
@@ -6,14 +6,12 @@ import data from './data/ghibli/ghibli.js';
 
 console.log(ghibliData, data);
 
-//const film = data.result; este no nos funcionó, pero lo dejo como registro
-
 //------------SECCIÓN CONSTANTES de LA DATA
 
-const movie = data.films; //declaramos la constante que nos guarda (o accede a) las películas
-console.log(movie);
+const films = data.films; //declaramos la constante que nos guarda (o accede a) las películas
+//console.log(films);
 
-const characters = movie.people;
+const characters = films.people;
 console.log(characters);
 
 
@@ -51,52 +49,18 @@ searchButton.addEventListener('click', (event) => {
 
 //-----------------USAR FOREACH PARA IMPLEMENTAR DATA DE LAS PELÍCULAS EN EL CONTENEDOR DE DATABASE
 
-movie.forEach()
+films.forEach(movie => {
 
-
-//let movieAndDirector = '';
-//Usamos un ciclo for para recorrer el array de películas y agregar al dom películas y posters
-for(let i = 0; i < movie.length; i++) {
-
-  const theFilm = movie[i];//le pedimos que nos indique los títulos de las películas
-  //console.log(theFilm);
-
-  const filmDirector = movie[i].director; //le pedimos que nos traiga los directores de las películas
-  //console.log(filmDirector);
-  //movieAndDirector = 'The movie ' + movie[i].title +' is directed by '+ movie[i].director; //Concatenar la data (PENDIENTE, estructurar para presentar al usuario)
-  //console.log(movieAndDirector);
-
-
-
-  //Declaro la constante que genera el div
+  //Se crea el div que contendrá el título de cada película y su respectiva portada
   const movieDiv = document.createElement('div');
+
+  //Se usar innerHTML para crear la estructura hmtl
   movieDiv.innerHTML += 
-  `<h4 class = "movie-box" id = "${theFilm.title}"> ${theFilm.title} <h4>
-  <img class="movie-poster" src="${theFilm.poster}" id = "${theFilm.id}">`
+  `<h4 class="movie-title" id="${movie.title}">${movie.title}<h4>
+  <img class="movie-poster" src="${movie.poster}" id = "${movie.id}">`
 
+  //se usa la función appendChild sobre el dataBaseContainer para introducir en éste el div recién creado
   dataBaseContainer.appendChild(movieDiv);
-  
-  //const movieList = document.createElement('li'); //crear constante que cree el elemento de lista
 
-  //const moviePoster = document.createElement('img'); //crear constante que cree las imágenes
+});
 
-  //dataBaseContainer.classList.add("movie-list"); //Dándole una clase a la lista de películas
-  //console.log(dataBaseContainer.className);
-
-  //const posterGallery = document.createElement('div'); //creamos un div para cada película y director
-  
-  //posterGallery.classList.add('poster-gallery'); //Asignamos una clase para el div de película y director
-  
-  //moviePoster.src = movie[i].poster; //se extrae el src de movie poster de cada película
-
-  //moviePoster.style.maxWidth = '200px'; //Configuramos tamaño de posters
-
-  //movieList.textContent = movieAndDirector; //para mostrar en pantalla el título y director de la película
-
-  //dataBaseContainer.appendChild(movieList); //Crea la lista de películas al DOM
-
-  //dataBaseContainer.appendChild(moviePoster); //Agregamos los poster al DOM
-
-  //dataBaseContainer.appendChild(posterGallery);
-
-} //Aquí termina el for del array de las películas
