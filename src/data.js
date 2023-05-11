7d2b08a2a8fd85a210585d226a17566b7d49a37d
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-05-11 09:27:21 -0400

    Funciona filterCharacter, filterLocalities

--- src/data.js
+++ src/data.js
@@ -4,6 +4,62 @@ export const ghibliData = () => {
   return 'ghibliData';
 };
 
-export const anotherExample = () => {
+/*export const anotherExample = () => {
   return 'OMG';
 };
+*/
+/*export const filterMovie = (data, propertyValue) => {
+  const films = data.filter(movie => 
+    movie.title === propertyValue);
+    return films;
+  
+};
+console.log(filterMovie, propertyValue);*/
+
+/*export const filtrarDirector = (data, seleccionDirector) => {
+  const filtro = data.ghibli.filter(x=>x.director.toLowerCase().includes(seleccionDirector))
+  console.log(filtrarDirector)
+}*/
+
+/*export const filterDirector = (data, seleccionDirector) => {
+  const filtro = data.films.filter(x=>x.director.includes(seleccionDirector))
+  console.log(filtro)
+}
+
+
+export const filterMovie = (data, propertyValue) => {
+  const films = data.filter(movie => movie.title === propertyValue);
+  return films;
+};
+*/
+export const filterCharacter = (films) => {
+  let characters = [];
+
+  for (const film of Object.values(films)) {
+      for (const character of film.people) {
+        characters.push({ name: character.name, img: character.img });
+      
+    }
+  }
+
+  return characters;
+};
+
+export const filterLocalities = (films) => {
+  let localities = [];
+
+  for (const film of Object.values(films)) {
+      for (const locality of film.locations) {
+        localities.push({ name: locality.name, img: locality.img });
+      }
+  }
+
+  return localities;
+};
+//Devuelve la lista de personajes
+  
+
+
+//funcion para mostrar personaje según película
+
+//
--- src/main.js
+++ src/main.js
@@ -1,75 +1,110 @@
 // eslint-disable-next-line no-unused-vars
-import { ghibliData } from './data.js'; // importa la función...
+import { ghibliData, filterCharacter, filterLocalities} from './data.js'; // importa la función...
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(ghibliData, data);
+//------------SECCIÓN CONSTANTES de LA DATA
 
-//const film = data.result; este no nos funcionó, pero lo dejo como registro
 
-//------------SECCIÓN CONSTANTES QUE TRAEN LA DATA
-const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log('hihihi');
-  });
-//declaramos la constante que nos guarda (o accede a) las películas
-const movie = data.films;
-console.log(movie);
+// Manipulación del DOM con los nombres obtenidos:
+
+const films = data.films; //declaramos la constante que nos guarda (o accede a) las películas
+//console.log(films);
+
 
 //------------SECCIÓN CONSTANTES DE INTERACCIÓN CON DOM
 
 const dataBaseContainer = document.getElementById('database-container'); //Declaración de la variable que contendra los elementos a insertar en el hmtl
 
+//const searchButton = document.getElementById('search-button'); //Constante del botón de búsqueda
+
+// eslint-disable-next-line no-unused-vars
+const localityButton = document.getElementById('locality-button'); //constante del botón de localidades
+
+// eslint-disable-next-line no-unused-vars
+const charButton = document.getElementById('character-button'); //Constante del botón de personajes
+
+// eslint-disable-next-line no-unused-vars
+const directorButton = document.getElementById('director-button'); //Constante del botón de directores
+
+
+//------------LA CONSTANTE PARA OCULTAR LA SECCIÓN DE BIENVENIDA
 const hideWelcome = document.querySelector('.welcome'); //Declaramos constante que guarda la sección welcome
 hideWelcome.style.display = 'none'; //ocultamos sección welcome
 
 
+//INTERMINADO
 
-//let movieAndDirector = '';
-//Usamos un ciclo for para recorrer el array de películas y agregar al dom películas y posters
-for(let i = 0; i < movie.length; i++) {
+//Se inicializa con el click
+charButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    //Vaciar contenedor
+    dataBaseContainer.innerHTML = "";
+    //Declara funcion con parametro
+    const characters = filterCharacter(films);
+    //Recorre los characters
+    characters.forEach((character) => {
+      // Crear un div para cada personaje
+      const charDiv = document.createElement('div');
+  
+      // Inyectar el HTML para mostrar el nombre y la imagen del personaje
+      charDiv.innerHTML = `
+        <h4 class="character-name" id="${character.name}">${character.name}</h4>
+        <img class="character-img" src="${character.img}" alt="${character.img}">
+      `;
+  
+      // Agregar el div del personaje al databaseContainer
+      dataBaseContainer.appendChild(charDiv);
+    });
+  });
 
-  const theFilm = movie[i];//le pedimos que nos indique los títulos de las películas
-  console.log(theFilm);
+  localityButton.addEventListener('click', (event) => {
+    event.preventDefault(event);
 
-  const filmDirector = movie[i].director; //le pedimos que nos traiga los directores de las películas
-  console.log(filmDirector);
-  //movieAndDirector = 'The movie ' + movie[i].title +' is directed by '+ movie[i].director; //Concatenar la data (PENDIENTE, estructurar para presentar al usuario)
-  //console.log(movieAndDirector);
+    //Vaciar contenedor
+    dataBaseContainer.innerHTML = "";
+    //Declara funcion con parametro
+    const localities = filterLocalities(films);
+    //Recorre los characters
+    localities.forEach((location) => {
+      //Crea el div
+        const localDiv = document.createElement('div');
+      //Inyecta html name + img
+        localDiv.innerHTML = `
+          <h4 class="local-name" id="${location.name}">${location.name}</h4>
+          <img class="local-img" src="${location.img}" id=${location.name}>
+        `;
+        //Se envía al div
+        dataBaseContainer.appendChild(localDiv);
+      });
+      //se muestra en consola
+      console.log(filterLocalities(films.location))
+  });
+//_---------------------------------------------------------------------------------------------
+
+directorButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const directoresFilter = searchDirector(films, 'Hayao Miyazaki')
+  console.log(directoresFilter);
+
+});
 
 
+//-----------------USAR FOREACH PARA IMPLEMENTAR DATA DE LAS PELÍCULAS EN EL CONTENEDOR DE DATABASE
 
-  //Declaro la constante que genera el div
+films.forEach(movie => {
+
+  //Se crea el div que contendrá el título de cada película y su respectiva portada
   const movieDiv = document.createElement('div');
+
+  //Se usar innerHTML para crear la estructura hmtl
   movieDiv.innerHTML += 
-  `<h4 class = "movie-box" id = "${theFilm.title}"> ${theFilm.title} <h4>
-  <img class="movie-poster" src="${theFilm.poster}" id = "${theFilm.id}">`
+  `<h4 class="movie-title" id="${movie.title}">${movie.title}<h4>
+  <img class="movie-poster" src="${movie.poster}" id = "${movie.id}">`
 
+  //se usa la función appendChild sobre el dataBaseContainer para introducir en éste el div recién creado
   dataBaseContainer.appendChild(movieDiv);
-  
-  //const movieList = document.createElement('li'); //crear constante que cree el elemento de lista
 
-  //const moviePoster = document.createElement('img'); //crear constante que cree las imágenes
-
-  //dataBaseContainer.classList.add("movie-list"); //Dándole una clase a la lista de películas
-  //console.log(dataBaseContainer.className);
-
-  //const posterGallery = document.createElement('div'); //creamos un div para cada película y director
-  
-  //posterGallery.classList.add('poster-gallery'); //Asignamos una clase para el div de película y director
-  
-  //moviePoster.src = movie[i].poster; //se extrae el src de movie poster de cada película
-
-  //moviePoster.style.maxWidth = '200px'; //Configuramos tamaño de posters
-
-  //movieList.textContent = movieAndDirector; //para mostrar en pantalla el título y director de la película
-
-  //dataBaseContainer.appendChild(movieList); //Crea la lista de películas al DOM
-
-  //dataBaseContainer.appendChild(moviePoster); //Agregamos los poster al DOM
-
-  //dataBaseContainer.appendChild(posterGallery);
-
-}
+});
